73c5db5aa12ad03d0d010b2cf9650ac9aaccc199
coderRaj07 <12131501.edu@gmail.com>
2024-04-29 17:43:41 +0530

    Refactor: Updated UserRepository.ts to handle user verification and error handling

--- user-service/app/repository/userRepository.ts
+++ user-service/app/repository/userRepository.ts
@@ -45,12 +45,42 @@ export class UserRepository extends CRUDOperations<UserModelAttributes> {
     }
   }
 
-  async updateVerification(userId: number, verificationCode: number, expiry: Date): Promise<void> {
+  async updateVerificationCode(userId: number, verificationCode: number, expiry: Date): Promise<void> {
     try {
+      // update only if user not verified
+      // else throw error user already verified
+      // Once user verifed always verified, 
+      // it is an OTP scene where the code is valid for a certain time 
+      // (30 mins to 1 yr)
+      const user = await this.find({ user_id: userId });
+      if (user.verified) {
+        throw new Error('User already verified');
+      }
+
       await this.update(
         { user_id: userId },
         { verification_code: verificationCode, expiry }
       );
+    } catch (error) {
+      console.error(error);
+      throw new Error(`Could not update verification information: ${error.message}`);
+    }
+  }
+
+  async updateVerifyUser(userId: number): Promise<void> {
+    try {
+      // check if user not verified
+      // then verify
+      // else throw error user already verified
+      const user = await this.find({ user_id: userId });
+      if (user.verified) {
+        throw new Error('User already verified');
+      }
+      await this.update(
+        { user_id: userId },
+        { verified: true }
+      );
+
     } catch (error) {
       console.error(error);
       throw new Error(`Could not update verification information: ${error.message}`);
