85b1487fb4bcadbc8ccb3493357fef1c6539d769
coderRaj07 <12131501.edu@gmail.com>
2024-04-29 17:43:11 +0530

    Refactor: Added VerificationInput and TimeDifference to userService.ts

--- user-service/app/service/userService.ts
+++ user-service/app/service/userService.ts
@@ -9,6 +9,8 @@ import { autoInjectable } from "tsyringe";
 import { SendVerificationCode } from "../utility/notification";
 import { GetHashedPassword, GetSalt, GetToken, ValidatePassword, VerifyToken } from "../utility/password";
 import { GenerateAccessCode } from "../utility/notification";
+import { VerificationInput } from "../models/dto/UpdateInput";
+import { TimeDifference } from "../utility/dateHelper";
 
 @autoInjectable()
 export class UserService {
@@ -16,6 +18,10 @@ export class UserService {
 
     constructor(repository: UserRepository) {
         this.repository = repository;
+    }
+
+    async ResponseWriteError(event: APIGatewayProxyEventV2) {
+        return ErrorResponse(404, "Request Method is not supported")
     }
 
     async CreateUser(event: APIGatewayProxyEventV2) {
@@ -53,7 +59,7 @@ export class UserService {
             const data = await this.repository.findAccount(input.email);
             const verified = await ValidatePassword(input.password, data.password, data.salt);
             if (!verified) { return ErrorResponse(404, "Invalid Password"); }
-            if(!verified){
+            if (!verified) {
                 throw new Error("Invalid Password")
             }
             const token = GetToken(data);
@@ -69,17 +75,53 @@ export class UserService {
     async GetVerificationToken(event: APIGatewayProxyEventV2) {
         const token = event.headers.authorization;
         const payload = await VerifyToken(token);
-        // Twilio verification
-        if (payload) {
-            const {code, expiry} = GenerateAccessCode();
-            // save on DB to confirm verification
-            const response = await SendVerificationCode(code, payload.phone);
-            return SuccessResponse(payload)
+        if (!payload) {
+            return ErrorResponse(404, "Authorisation Failed")
         }
-        return SuccessResponse({ message: "response from verify user" })
+        const { code, expiry } = GenerateAccessCode();
+
+        // save on DB to confirm verification
+        await this.repository.updateVerificationCode(payload.user_id, code, expiry);
+        
+        // To be uncommented for production 
+        // to send verification code to Phone
+        // await SendVerificationCode(code, payload.phone);
+
+        return SuccessResponse({ message: "Verification code is sent to the mobile number" })
     }
 
     async VerifyUser(event: APIGatewayProxyEventV2) {
+        const token = event.headers.authorization;
+        const payload = await VerifyToken(token);
+        if (!payload) {
+            return ErrorResponse(404, "Authorisation Failed")
+        }
+
+        const input = plainToClass(VerificationInput, event.body);
+        const error = await AppValidationError(input);
+        if (error) {
+            return ErrorResponse(404, error);
+        }
+
+        // find the user account
+        const { verification_code, expiry } = await this.repository.findAccount(payload.email);
+        if (verification_code === parseInt(input.code)) {
+            // check expiry 
+            const currentTime = new Date();
+            const diff = TimeDifference(expiry.toISOString(), currentTime.toISOString(), "m");
+            if (diff > 0) {
+                console.log(diff, "TIME DIFFERENCE")
+                console.log("Verified Successfully")
+                // update on db
+                await this.repository.updateVerifyUser(payload.user_id);
+            }
+            else {
+                return ErrorResponse(404, "Verification code has expired")
+            }
+
+
+        }
+
         return SuccessResponse({ message: "response from verify user" })
     }
 
