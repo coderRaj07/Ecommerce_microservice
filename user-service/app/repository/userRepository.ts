db72dc07bf68919e1ee1b3c7d6d16ec7ca3020ac
coderRaj07 <12131501.edu@gmail.com>
2024-05-01 06:51:56 +0530

    Refactor: Import ProfileInput from index

--- user-service/app/repository/userRepository.ts
+++ user-service/app/repository/userRepository.ts
@@ -1,25 +1,5 @@
-// import { UserModel } from "app/models/UserModel";
 
-// export class UserRepository {
-//     constructor() { }
-
-//     async createAccount({ email, password, salt, phone, userType }: UserModel) {
-//         const client = await DBClient();
-//         await client.connect();
-//         const queryString =
-//             "INSERT INTO users(email,password,salt,phone,user_type) VALUES($1,$2,$3,$4,$5) RETURNING *";
-//         const values = [email, password, salt, phone, userType];
-//         const result = await client.query(queryString, values);
-//         await client.end();
-//         if (result.rowCount > 0) {
-//             return result.rows[0] as UserModel;
-//         }
-//     }
-
-// }
-
-
-import { ProfileInput } from "../models/dto/AddressInput";
+import { ProfileInput } from "../models/dto";
 import { UserModel, UserModelAttributes } from "../models/UserModel";
 import { CRUDOperations } from "./dbOperation";
 import { AddressRepository } from "./addressRepository";
